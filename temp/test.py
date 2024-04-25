afe29fb4ef97a6549bef83247132c69ad60252f1
Gabriel Lindeblad <Gabriel.lindeblad@icloud.com>
2024-04-25 13:36:18 +0200

    Setup and implemented CBC string encryption tests.
    Signed-off-by: Gabriel Lindeblad <Gabriel.lindeblad@icloud.com>

--- temp/test.py
+++ temp/test.py
@@ -1,17 +1,24 @@
 import numpy as np
 from AES_Python import AES
 
-aes_test = AES(running_mode="CBC")
+aes_test = AES(running_mode="CBC",
+               key="2b7e151628aed2a6abf7158809cf4f3c",
+               iv="000102030405060708090A0B0C0D0E0F"
+               )
 
 print(aes_test, "\n")
 
-data = 'En lång text med en massa tecken som behöver hantaeras separat och kräver flera rundor'
+data_raw = '6bc1bee22e409f96e93d7e117393172a'
+
+data = "".join([chr(i) for i in bytes.fromhex(data_raw)])
 
 print("Original data:", data)
 
 enc_data = aes_test.enc(data_string=data)
 
-print("Encrypted data:", enc_data)
+print("Encrypted data:", bytearray([ord(i) for i in enc_data]).hex())
+
+print("Expected:", "7649abac8119b246cee98e9b12e9197d")
 
 dec_data = aes_test.dec(data_string=enc_data)
 
--- tests/test_dec.py
+++ tests/test_dec.py
@@ -66,3 +66,42 @@ def test_enc_ecb(data, key, expected):
     aes = AES(key=key)
 
     assert aes.dec(data_string=data.decode("utf-8")) == expected
+
+
+@pytest.mark.parametrize("data,key,iv,expected", [
+    # 128 bit
+    ('7649abac8119b246cee98e9b12e9197d', "2b7e151628aed2a6abf7158809cf4f3c",
+     "000102030405060708090a0b0c0d0e0f", "6bc1bee22e409f96e93d7e117393172a"),
+    ("5086cb9b507219ee95db113a917678b2", "2b7e151628aed2a6abf7158809cf4f3c",
+     "7649ABAC8119B246CEE98E9B12E9197D", 'ae2d8a571e03ac9c9eb76fac45af8e51'),
+    ("73bed6b8e3c1743b7116e69e22229516", "2b7e151628aed2a6abf7158809cf4f3c",
+     "5086CB9B507219EE95DB113A917678B2", '30c81c46a35ce411e5fbc1191a0a52ef'),
+    ("3ff1caa1681fac09120eca307586e1a7", "2b7e151628aed2a6abf7158809cf4f3c",
+     "73BED6B8E3C1743B7116E69E22229516", 'f69f2445df4f9b17ad2b417be66c3710'),
+    # 192 bit
+    ("4f021db243bc633d7178183a9fa071e8", "8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7b",
+     "000102030405060708090A0B0C0D0E0F", '6bc1bee22e409f96e93d7e117393172a'),
+    ("b4d9ada9ad7dedf4e5e738763f69145a", "8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7b",
+     "4F021DB243BC633D7178183A9FA071E8", 'ae2d8a571e03ac9c9eb76fac45af8e51'),
+    ("571b242012fb7ae07fa9baac3df102e0", "8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7b",
+     "B4D9ADA9AD7DEDF4E5E738763F69145A", '30c81c46a35ce411e5fbc1191a0a52ef'),
+    ("08b0e27988598881d920a9e64f5615cd", "8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7b",
+     "571B242012FB7AE07FA9BAAC3DF102E0", 'f69f2445df4f9b17ad2b417be66c3710'),
+    # 256 bit
+    ("f58c4c04d6e5f1ba779eabfb5f7bfbd6", "603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4",
+     "000102030405060708090A0B0C0D0E0F", '6bc1bee22e409f96e93d7e117393172a'),
+    ("9cfc4e967edb808d679f777bc6702c7d", "603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4",
+     "F58C4C04D6E5F1BA779EABFB5F7BFBD6", 'ae2d8a571e03ac9c9eb76fac45af8e51'),
+    ("39f23369a9d9bacfa530e26304231461", "603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4",
+     "9CFC4E967EDB808D679F777BC6702C7D", '30c81c46a35ce411e5fbc1191a0a52ef'),
+    ("b2eb05e2c39be9fcda6c19078c6a9d1b", "603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4",
+     "39F23369A9D9BACFA530E26304231461", 'f69f2445df4f9b17ad2b417be66c3710'),
+])
+def test_dec_cbc(data, key, iv, expected):
+    aes = AES(running_mode="CBC", key=key, iv=iv)
+
+    data = "".join([chr(i) for i in bytes.fromhex(data)])
+
+    enc_data = aes.dec(data_string=data)
+
+    assert bytearray([ord(i) for i in enc_data]).hex() == expected
